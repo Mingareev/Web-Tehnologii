1b3137c8212f61c06d68bc3c7315d73ecf9a2040
Mingareev <81474718+Mingareev@users.noreply.github.com>
2023-02-16 02:29:57 +0500

    Загрузил код 2 лабы

---
+++ 2/index.php
@@ -0,0 +1,17 @@
+<html>
+<head> <title> Лаба2 </title> </head>
+<div align="left">
+</div>
+<body align="center">
+<h1>Лабораторная работа №2</h1><p>
+<h2>Мингареев Радмир Адикович ПИ-423</h2><p>
+<u1>
+        <li><a href="http://a0777012.xsph.ru/js/2/Lab1.html">Задание 1. </a></li>
+        <li><a href="http://a0777012.xsph.ru/js/2/Lab2.html">Задание 2. </a></li>
+        <li><a href="http://a0777012.xsph.ru/js/2/Lab3.html">Задание 3. </a></li>
+        <li><a href="http://a0777012.xsph.ru/js/2/Lab4.html">Задание 4. </a></li>
+        <li><a href="http://a0777012.xsph.ru/js/2/Lab5.html">Задание 5. </a></li>
+        <li><a href="http://a0777012.xsph.ru/js/2/Lab6.html">Задание 6. </a></li>
+        <li><a href="http://a0777012.xsph.ru/js/2/Lab7.html">Задание 7. </a></li>
+</u1>
+</html>
